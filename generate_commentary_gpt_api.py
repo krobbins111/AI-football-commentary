a0868df96e46de979059c95fbf2739b088be9ff1
KEVINS_DESKTOP\kevin <kevin.robbins14@gmail.com>
2023-11-26 16:36:15 -0500

    cleanup

--- generate_commentary_gpt_api.py
+++ generate_commentary_gpt_api.py
@@ -38,12 +38,10 @@ flags.DEFINE_bool('real_time', True,
                   'If true, environment will slow down so humans can play.')
 
 all_commentary = []
-full_steps = 3000 * 9
-current_10_min = 9
 api_response = ''
 
 def get_chat_completion(prompt):
-    client = OpenAI(api_key='')
+    client = OpenAI(api_key='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')
     print('start inner thread')
     completion = client.chat.completions.create(
     # model="ft:gpt-3.5-turbo-0613:personal::8OqM7Fzo",
@@ -74,14 +72,8 @@ def threaded_inference(prompt, interrupt_current_commentary, steps_time): # RIGH
     print("=" * 80)
     print('prompt: ' + prompt)
     print('model o/p: ' + api_response)
-    # print('model o/p: ' + str(text.strip().find('.')))
     print("=" * 80)
-    global current_10_min
     all_commentary.append((api_response, interrupt_current_commentary, steps_time))
-    # all_commentary.append((text, (3000 - steps_left) // 100 * current_10_min))
-    print(current_10_min)
-    # if steps_left == 0:
-    #     current_10_min -= 1
     
 
 def get_most_recent_avi(directory_path):
@@ -129,12 +121,6 @@ def concatenate_audio_from_paths(video_path, audio_paths, steps_time):
         actual_pre_silence_time = (video_clip.duration / steps_time) * int(pre_silence_time)
         actual_start_time = (video_clip.duration / steps_time) * int(start_time)
         print('total time:', total_time, 'last played at:', last_played_time)
-        # if interrupt and audio_clips_with_silence:
-        #     last_played_time = actual_pre_silence_time + last_duration
-        #     audio_clips_with_silence[-1] = concatenate_audioclips([audio_clips_with_silence[-1].subclip(0, actual_start_time - last_played_time), current_clip])
-        #     last_duration = current_clip.duration
-        #     total_time += audio_clip_with_silence.duration
-        # elif not last_played_time > actual_start_time:
         if not last_played_time > actual_start_time:
             # Generate silence before the current clip
             last_played_time = actual_pre_silence_time + last_duration
@@ -203,11 +189,8 @@ def record_and_save(duration=31):
     print(f"Audio saved as {filename.replace('wav', 'mp3')}")
 
 
-example_commentary = ["And we're off! The match has officially begun with a swift kick-off, setting the tone for an exciting game ahead.", "And it looks like he's lining up to take it, let's see if he can get it past the wall and into the back of the net!", 'The player has been fouled, and now they have a great opportunity to score from this free kick. Things are about to get interesting!', 'The referee is giving a free kick, an opportunity for the team to try and score from a set piece.', "What a missed opportunity for the attacking team! They couldn't convert and it's now a goal kick.", "And it's a corner kick! The attacking team will have a chance to create a scoring opportunity from this set-piece.", 'The referee has awarded a free kick! This is a great opportunity for the team to capitalize and create a scoring chance.', "Great pressure from the attacking team! They've earned themselves a corner kick to keep up the momentum.", "The attacking team couldn't convert their chances into a goal, and now it's a goal kick for the opposing team.", "The attacking team's efforts were in vain as the ball crossed the goal line, resulting in a goal kick for the defending team.", 'What a fantastic strike! The away team has taken the lead with a clinical finish.', "What a disappointment for the attacking team! After all that build-up play, it's just a goal kick for the opposition.", "The attacking team had a promising build-up but ultimately didn't find the mark. It's just a goal kick for the defending team.", "And the player lines up for the shot... it's a strong strike, but the goalkeeper dives and makes an incredible save!", "The goalkeeper wasn't even troubled by that shot, it was an easy save for him."]
-
-
 def main(_):
-    set_api_key('')
+    set_api_key('199b9659c6f6f79b1c758af9e63ce4d6')
     cfg = config.Config({
         'env_name': FLAGS.env_name,
         'action_set': FLAGS.action_set,
@@ -224,14 +207,14 @@ def main(_):
         cfg['level'] = FLAGS.level
 
     # record voice into mp3
-    # print('Prepare to speak for voice training data in...')
-    # print(3)
-    # time.sleep(1)
-    # print(2)
-    # time.sleep(1)
-    # print(1)
-    # time.sleep(1)
-    # record_and_save()
+    print('Prepare to speak for voice training data in...')
+    print(3)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    print(1)
+    time.sleep(1)
+    record_and_save()
     voice = clone(
         name = "cloned_voice",
         files = [os.path.join('raw_audio', 'voice_training.mp3')] 
@@ -250,18 +233,7 @@ def main(_):
             observation, reward, done, info = env.step(action)
             print(env._env._steps_time, env._env._step_count)
             prompt, interrupt_current_commentary = commentary.process_observation(observation)
-            # done = True
-            # print(observation[0]['steps_left'])
-            # if prompt:
-            #     if commentary_thread.is_alive():
-            #         if interrupt_current_commentary:
-            #             # commentary_thread kill/stop
-            #             # commentary_thread.start()
-            #             print('thread alive')
-            #     else:
-            #         commentary_thread = threading.Thread(target=threaded_inference,
-            #                                                 args=(prompt, env._env._steps_time))
-            #         commentary_thread.start()
+            
             for _ in range(1):
                     if prompt:
                         threading.Thread(target=threaded_inference, args=(prompt, interrupt_current_commentary, env._env._steps_time)).start()
@@ -269,21 +241,8 @@ def main(_):
                 print('Done with game')
                 print(all_commentary)
                 time.sleep(10)
-                # tts_engine = pyttsx3.init()
-                # tts_engine.setProperty('voice', 'english')
-                # tts_engine.setProperty('rate', 200)
-                gen_count = 0
                 for commentary, interrupt_current_commentary, second in all_commentary:
                     interrupt_string = "interrupt" if interrupt_current_commentary is True else "normal"
-                    # tts_engine.save_to_file(commentary, f'{second}.{interrupt_string}.mp3')
-                    # tts_engine.runAndWait()
-                    # if gen_count % 2 == 0:
-                    #     audio = generate(text=commentary, voice=Voice(voice_id='6SyeRjC0kKHiLuScI65N'), model="eleven_multilingual_v2")
-                    #     save(audio, f'{second}.{interrupt_string}.mp3')
-                    # else:
-                    #     audio = generate(text=commentary, voice=Voice(voice_id='z4COigt0SkX79fhgEDzg'), model="eleven_multilingual_v2")
-                    #     save(audio, f'{second}.{interrupt_string}.mp3')
-                    # gen_count = gen_count + 1
                     audio = generate(text=commentary, voice=voice, model="eleven_multilingual_v2")
                     save(audio, f'{second}.{interrupt_string}.mp3')
 
